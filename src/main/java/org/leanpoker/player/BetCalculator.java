c808e9dbc78369acaaeecbc10196cde841cc82e8
Max-Bevelander-Miro <max.b@miro.com>
2024-01-10 12:37:07 +0100

    Min amount for all-in/call / all-in/raise

--- src/main/java/org/leanpoker/player/BetCalculator.java
+++ src/main/java/org/leanpoker/player/BetCalculator.java
@@ -3,11 +3,12 @@ package org.leanpoker.player;
 public class BetCalculator {
 
     public int calculate(GameState gameState, Bet bet) {
+        int remaining = gameState.getPlayers().get(gameState.getIn_action()).getStack();
         switch (bet) {
             case RAISE:
-                return getCall(gameState) + gameState.getMinimum_raise();
+                return Math.min(remaining, getCall(gameState) + gameState.getMinimum_raise());
             case MATCH:
-                return getCall(gameState);
+                return Math.min(remaining, getCall(gameState));
             case FOLD:
                 return 0;
             default:
